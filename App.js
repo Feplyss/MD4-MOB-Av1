da331aed5f0f00e3086032e3d3ee6d1739a4227e
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-10-03 20:20:58 -0300

    1

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Input, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber] = useState('');
+
+  function fatorial(num){
+    var res = 1;
+    for(var i = num; i > 0; i--){
+      res = res * i
+    }
+    return (<Text>{res}</Text>);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="numero" onChange={setNumber} value={number}/>
+      <Button title="Calcular" onPress={() => fatorial({number})}></Button>
+      <Text>{number}</Text>
     </View>
   );
 }
